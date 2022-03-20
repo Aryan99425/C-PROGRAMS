e11af5608579f39c4fd6d3417d4ca42afc960b5b
Aryan99425 <101926061+Aryan99425@users.noreply.github.com>
2022-03-20 12:50:58 +0530

    Add files via upload

---
+++ 20.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#define PI 3.14
+
+int main()
+{
+    float radius,area;
+    printf("ARYAN THAKUR")
+    printf("Enter the radius of circle:\n");
+    scanf("%f",&radius);
+    area = PI*radius*radius;
+    printf("Area of circle:%.2f",area);
+
+    return 0;
+}
